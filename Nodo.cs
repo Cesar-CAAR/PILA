4f53308a19bf63f1b0893ef5fe4dc79bdf5feb98
Cesar-CAAR <cesar.andres5242@gmail.com>
2024-10-31 10:51:09 -0700

    pila

---
+++ Nodo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_de_Pila
+{
+    internal class Nodo
+    {
+        private int valor;
+        private Nodo sig;
+
+        public Nodo(int Valor)
+        {
+            valor = Valor;
+            sig = null;
+        }
+
+        public int Valor { get => valor; set => valor = value; }
+        internal Nodo Sig { get => sig; set => sig = value; }
+    }
+}
